a68c76f7a5c8c0fc3c47254d4f4888f60a93a972
Chester Corpuz <chestercorpuzz@gmail.com>
2019-05-07 14:38:57 +0400

    Sticky navigation

--- js/index.js
+++ js/index.js
@@ -1,3 +1,16 @@
+window.onscroll = function() {myFunction()};
+
+var nav = document.getElementById("nav");
+var sticky = nav.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    nav.classList.add("sticky")
+  } else {
+    nav.classList.remove("sticky");
+  }
+}
+
 //	Changes like and unlike button colours
 function activate(choice){
 	if (choice === "like") {
