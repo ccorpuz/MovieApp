74c2e088c9bd8c7c8eebb5572a9d775161b29850
Chester Corpuz <chestercorpuzz@gmail.com>
2019-05-09 03:50:17 +0400

    Simple loading animation

--- js/cast_member.js
+++ js/cast_member.js
@@ -1,6 +1,15 @@
 //	Loading animation
 //	Check if cast info is in session storage
 
+function runLoader(){
+	setTimeout(loadPage, 1500);
+}
+
+function loadPage(){
+	document.getElementById("loader").style.display = "none";
+	document.getElementById("container").style.display = "block";
+}
+
 function goBack() {
 	history.go(-1);
 }
