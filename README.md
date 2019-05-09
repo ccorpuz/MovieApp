bf1d2f8ceed521e05425044f38f3d5e65235acbb
Chester Corpuz <chestercorpuzz@gmail.com>
2019-05-09 07:17:57 +0400

    Final commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-A movie viewer web app
+# Movie App
+
+Made with vanilla HTML, CSS, JavaScript. No libraries, no cdn links, not even flexbox and grid (Trying to support old versions of ~the most popular browser~ IE).
--- js/cast.js
+++ js/cast.js
@@ -4,10 +4,17 @@ function redirectCast(id){
 	location.href = "./cast_member.html";
 }
 
+//	Add listeners for redirect
 var castClasses = document.getElementsByClassName("cast_card");
 
 for (var i = 0; i < castClasses.length; i++) {
 	if (i != 2) {
 		castClasses[i].addEventListener('click', redirectCast.bind(this, castClasses[i].id), false);
+	}
+}
+
+function poof(){
+	for(var i = 1; i < castClasses.length; i+=2){
+		castClasses[i].classList.add("poof");
 	}
 }
